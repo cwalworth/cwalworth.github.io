f483080d5e953ed4f3100f20f96e6d00cd99add5
Cameron Walworth <cameron.walworth@gmail.com>
2019-07-03 01:02:10 -0700

    add build

---
+++ build/precache-manifest.35dd4ddfc652d4f8f32cb2eb04a694b3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576272e7925bc6214aa3283b822bedaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c809993fc08539c35987",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "c809993fc08539c35987",
+    "url": "/static/js/2.08541029.chunk.js"
+  },
+  {
+    "revision": "d8317a0dc540a4ee4c2e",
+    "url": "/static/js/main.ee723d45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcdeba73283b9c4d0ddbdaa0ca87d162",
+    "url": "/static/media/ed_2017.bcdeba73.jpg"
+  },
+  {
+    "revision": "a12e919d3221618dac063a548378a52e",
+    "url": "/static/media/fronius_tpsi.a12e919d.jpg"
+  },
+  {
+    "revision": "8be8db7d538d65fb8346178e417e3a07",
+    "url": "/static/media/fronius_tpsi_robot.8be8db7d.jpg"
+  },
+  {
+    "revision": "954108d50bb9f9ec0791683857ddb65c",
+    "url": "/static/media/hermle.954108d5.jpg"
+  },
+  {
+    "revision": "8a1ef452b49c6a054a5f8b778de76350",
+    "url": "/static/media/lang.8a1ef452.jpg"
+  },
+  {
+    "revision": "e8e207b63fcdaa58210b272b14d95d95",
+    "url": "/static/media/lpw.e8e207b6.jpg"
+  },
+  {
+    "revision": "44d1d7cd4dff0e5840706e9c99eddbf6",
+    "url": "/static/media/magswitch.44d1d7cd.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2f0f67aff27d8cd290ff4825c1197b8e",
+    "url": "/static/media/slm.2f0f67af.jpg"
+  }
+]);
